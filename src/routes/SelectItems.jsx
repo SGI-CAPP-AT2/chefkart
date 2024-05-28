fe7ff34aa4cf82f611d78e3b366077fe4bef9695
Big-oh-learn <shubhamingale779@gmail.com>
2024-05-28 16:28:32 +0530

    added stories about popular dishes

--- src/routes/SelectItems.jsx
+++ src/routes/SelectItems.jsx
@@ -2,15 +2,33 @@ import DateTimeBanner from "../components/DateTimeBanner";
 import ScrollableChipsBox from "../components/ScrollableChipsBox";
 import chips_data from "../data/cats.json";
 import { useTitle } from "../context/title.context";
+import ScrollableStoriesBox from "../components/ScrollableStories";
+import { useEffect, useState } from "react";
 
 const SelectItems = () => {
   const setTitle = useTitle();
+  const [data, setData] = useState(null);
   setTitle("Select Dishes");
-  console.log(chips_data);
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetched = await fetch(
+        "https://8b648f3c-b624-4ceb-9e7b-8028b7df0ad0.mock.pstmn.io/dishes/v1/"
+      );
+      const json = await fetched.json();
+      setData(json);
+    };
+    fetchData();
+  }, []);
+  console.log(data);
   return (
     <div>
-      <DateTimeBanner />
-      <ScrollableChipsBox data={chips_data} />
+      {data && (
+        <>
+          <DateTimeBanner />
+          <ScrollableChipsBox data={chips_data} />
+          <ScrollableStoriesBox data={data.popularDishes} />
+        </>
+      )}
     </div>
   );
 };
