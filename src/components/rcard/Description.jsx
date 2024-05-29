2f2e2732a91a2e337ca35e1201314157f1064988
Shubham Ingale <77089227+SGI-CAPP-AT2@users.noreply.github.com>
2024-05-29 05:42:58 +0530

    deleted files in pages branche to add files of deployment

--- src/components/rcard/Description.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Description = styled.div`
-  color: #707070;
-  word-wrap: break-word;
-  font-size: 15px;
-  margin-top: 10px;
-`;
-export default Description;
